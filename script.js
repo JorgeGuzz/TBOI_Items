c8abee39336a8913dc4d61ac1037df02416e4f17
Jorge Guzman <jguzmanv01@uc.cl>
2022-11-24 13:00:12 -0300

    update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const height_bubble = HEIGHT_BUBBLE - MARGIN_BUBBLE.top - MARGIN_BUBBLE.bottom;
 
 
 d3.json("data.json").then((datos) => {
-    datos = datos.map(v => ({...v, image_url: `item_${v.ID}.png`}))
+    datos = datos.map(v => ({ ...v, image_url: `item_${v.ID}.png` }))
     console.log(datos);
 
     const vis = d3.select('#vis')
@@ -21,19 +21,32 @@ d3.json("data.json").then((datos) => {
     })
 
     const svg_bubble = d3.select("#bubble_vis")
-                        .style("border", "5px solid black")
-                        .style("border-radius", "5px")
-                        .append("svg")
-                        .attr("width", WIDTH_BUBBLE)
-                        .attr("height", HEIGHT_BUBBLE)
+        .style("border", "5px solid black")
+        .style("border-radius", "5px")
+        .append("svg")
+        .attr("width", WIDTH_BUBBLE)
+        .attr("height", HEIGHT_BUBBLE)
 
     const escalaBubble = d3
         .scalePow()
         .exponent(1 / 2)
         .domain([0, d3.max(datos, (d) => d.Quality)])
-        .rangeRound([25, 100]);
-    
-    
-    
+        .rangeRound([10, 50]);
+
+
+    const myNodes = datos.map(d => ({ ...d, radius: escalaBubble(d.Quality), x: Math.random() * width_bubble, y: Math.random() * height_bubble }))
+    console.log(myNodes)
+
+
+    // < defs >
+    //     <pattern id="image" x="0%" y="0%" height="100%" width="100%"
+    //         viewBox="0 0 512 512">
+    //         <image x="0%" y="0%" width="512" height="512" xlink:href="https://cdn3.iconfinder.com/data/icons/people-professions/512/Baby-512.png"></image>
+    //     </pattern>
+    // </defs >
+
+    // <circle id="sd" class="medium" cx="5%" cy="40%" r="5%" fill="url(#image)" stroke="lightblue" stroke-width="0.5%" />
+
+
 });
 
